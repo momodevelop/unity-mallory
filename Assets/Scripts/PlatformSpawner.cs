334281e6c2df5061d5ae724589fe14867d73c4f5
GERALD WONG <ramza88@gmail.com>
2019-11-02 14:18:43 +0800

    shifted some code, added level end trigger

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(Tilemap))]
-
 public class PlatformSpawner : Momo.PersistantMonoBehaviourSingleton<PlatformSpawner>
 {
-    public Sprite obstacleSprite;
-    
+    public class Tile : TileBase
+    {
+        public Sprite sprite;
+
+        public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+        {
+
+            base.GetTileData(position, tilemap, ref tileData);
+            if (sprite != null)
+            {
+                tileData.sprite = sprite;
+                tileData.colliderType = UnityEngine.Tilemaps.Tile.ColliderType.Sprite;
+            }
+        }
+
+        public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+        {
+            base.RefreshTile(position, tilemap);
+        }
+    }
+
+    public Sprite obstacleSprite;   
 
     const float kTileSize = 96;
     const float kPixelPerUnit = 100.0f;
@@ -20,16 +39,12 @@ public class PlatformSpawner : Momo.PersistantMonoBehaviourSingleton<PlatformSpa
     {
         if (obstacleSprite == null)
             Destroy(this);
+
         tilemap = GetComponent<Tilemap>();
         GenerateLevel();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    void GenerateLevel()
+    public void GenerateLevel()
     {
         int WIDTH = 20;
         int HEIGHT = 20;
@@ -51,25 +66,15 @@ public class PlatformSpawner : Momo.PersistantMonoBehaviourSingleton<PlatformSpa
             CHANCE_TO_PLACE_BLOCK
         );
 
-        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
-        float worldScreenWidth = (worldScreenHeight / Screen.height) * Screen.width;
-
-
-        
-        float startY = kTileSize / 2;
-
         for (int row = 0; row < map.GetLength(0); ++row)
         {
-            float startX = kTileSize / 2;
             for (int col = 0; col < map.GetLength(1); ++col)
             {
                 if (map[row, col] == 1)
                 {
                     SpawnPlatform(new Vector3Int(col, row, 0));
                 }
-                startX += kTileSize;
             }
-            startY += kTileSize;
         }
 
 
@@ -86,23 +91,3 @@ public class PlatformSpawner : Momo.PersistantMonoBehaviourSingleton<PlatformSpa
     }
 }
 
-public class Tile : TileBase
-{
-    public Sprite sprite;
- 
-    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
-    {
-
-        base.GetTileData(position, tilemap, ref tileData);
-        if (sprite != null)
-        {
-            tileData.sprite = sprite;
-            tileData.colliderType = UnityEngine.Tilemaps.Tile.ColliderType.Sprite;
-        }
-    }
-
-    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
-    {
-        base.RefreshTile(position, tilemap);
-    }
-}
